29fabe2586e60c63dcd4249b85f243bdb11de4aa
noted bones haver kaolin maia cash <git+kaolin@maia.cash>
2022-03-23 22:40:47 +0000

    Update backup.bat

--- phoebe/leto/apollo/aesklepios/epione/iaso/backup.bat
+++ phoebe/leto/apollo/aesklepios/epione/iaso/backup.bat
@@ -6,11 +6,12 @@ set hour=%time:~0,2%
 if %hour% lss 10 (set hour=0%time:~1,1%)
 set minute=%time:~3,2%
 set second=%time:~6,5%
-set slot="%year%-%month%-%day%@%hour%%minute%Hrs%second%s - TO MANUALLY RESTORE simply rename me and overwrite the existing save folder."
+set slot=%year%-%month%-%day%@%hour%%minute%Hrs%second%s
+set guide=" - TO MANUALLY RESTORE simply rename me and overwrite the existing save folder"
 
 set source=%APPDATA%\EldenRing
 set quicksave=%APPDATA%\EldenRingQuicksave
-set archive=%APPDATA%\EldenRingArchive\%slot%
+set archive=%APPDATA%\EldenRingArchive\%slot%%guide%
 
-robocopy %source% %quicksave% /E /is /it
-robocopy %source% %archive% /E /is /it
+robocopy %source% %quicksave% /E /IS /IT
+robocopy %source% %archive% /E /IS /IT
